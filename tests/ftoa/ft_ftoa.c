203cf08fc03c44f9de2ea41f88a9dd2f37f0bd4e
dpenney <behaqosimi@gmail.com>
2019-11-08 22:50:46 +0300

    Dividing to several functions ft_ftoa

--- tests/ftoa/ft_ftoa.c
+++ tests/ftoa/ft_ftoa.c
@@ -6,17 +6,16 @@
 /*   By: dpenney <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/08 20:13:48 by dpenney           #+#    #+#             */
-/*   Updated: 2019/11/08 20:17:05 by dpenney          ###   ########.fr       */
+/*   Updated: 2019/11/08 22:45:20 by dpenney          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int			ft_inter_len(long long inter)
+int	ft_inter_len(long long inter)
 {
-	int		i;
+	int				i;
 
 	i = 0;
 	if (inter <= 0)
@@ -32,83 +31,96 @@ int			ft_inter_len(long long inter)
 	return (i);
 }
 
-struct var_ftoa
+struct	varftoa
 {
-    long double fraction;
-    long long   inter;
-    long double tmp;
-    char        *str;
-    int         lenTillDot;
-    int				i;
-	int				tmp2;
-	int				lenAfDot;
+	long double		fraction;
+	long long		inter;
+	long double		tmp;
+	char			*str;
+	int				len_till_dot;
+	int				i;
+	int				len_af_dot;
 };
 
-char                *ft_rounding_up(long double fraction, char *str, int lenAfDot)
+char	*ft_rounding_up(long double fraction, char *str, int len_af_dot)
 {
-    int             o;
+	int				o;
 
-    o = 0;
-    if (fraction >= 0.5 && str[lenAfDot - 1] != '9')
-    {
-        str[lenAfDot - 1] += 1;
-    }
-    if (fraction >= 0.5 && str[lenAfDot - 1] == '9')
-    {
-        while(str[lenAfDot - 1] == '9')
-        {
-            str[lenAfDot - 1] = '0';
-            o++;
-        }
-    }
-    str[lenAfDot] = '\0';
-    return (str);
+	o = 0;
+	if (fraction >= 0.5 && str[len_af_dot - 1] != '9')
+	{
+		str[len_af_dot - 1] += 1;
+	}
+	if (fraction >= 0.5 && str[len_af_dot - 1] == '9')
+	{
+		while (str[len_af_dot - 1] == '9')
+		{
+			str[len_af_dot - 1] = '0';
+			o++;
+		}
+	}
+	str[len_af_dot] = '\0';
+	return (str);
 }
-char                *ft_ftoa(double num, long long p)
-{
-    struct var_ftoa            f;
 
-    f.i = 0;
-    f.inter = (long long)num;
-    f.tmp = (double)f.inter;
-    f.fraction = num - f.tmp;
-    f.lenTillDot = ft_inter_len(f.inter);
-    if (!(f.str = (char *)malloc(sizeof(char) * (f.lenTillDot + p + 2))))
-        return (NULL);
-    f.lenAfDot = f.lenTillDot + 1;
+char	*ft_fill_fraction(long double fraction, char *str, \
+			long long p, int len_af_dot)
+{
+	long long		tmp2;
+	int				i;
+
+	i = 0;
+	while (i < p)
+	{
+		fraction = fraction * 10;
+		tmp2 = (int)fraction;
+		str[len_af_dot++] = tmp2 + '0';
+		fraction = fraction - tmp2;
+		i++;
+	}
+	return (ft_rounding_up(fraction, str, len_af_dot));
+}
+
+char	*ft_ftoa(double num, long long p)
+{
+	struct varftoa	f;
+
+	f.i = 0;
+	f.inter = (long long)num;
+	f.tmp = (double)f.inter;
+	f.fraction = num - f.tmp;
+	f.len_till_dot = ft_inter_len(f.inter);
+	if (!(f.str = (char *)malloc(sizeof(char) * (f.len_till_dot + p + 2))))
+		return (NULL);
+    f.len_af_dot = f.len_till_dot + 1;
+//	ft_fill_str(f.str, f.len_till_dot, f.inter, f.i)
     if (f.inter == 0)
-    {
-        f.str[0] = '0';
-        f.str[f.lenTillDot] = '.';
-        f.lenTillDot++;
-    }
-    else
-    {
-        f.str[f.lenTillDot--] = '.';
-        while (f.i <= f.lenTillDot)
-        {
-            f.str[f.lenTillDot] = f.inter % 10 + '0';
-            f.inter /= 10;
-            f.lenTillDot--;
-        }
-    }
-    while (f.i < p)
-    {
-        f.fraction = f.fraction * 10;
-        f.tmp2 = (int)f.fraction;
-        f.str[f.lenAfDot++] = f.tmp2 + '0';
-        f.fraction = f.fraction - f.tmp2;
-        f.i++;
-    }
-    return (ft_rounding_up(f.fraction, f.str, f.lenAfDot));
+	{
+		f.str[0] = '0';
+		f.str[f.len_till_dot] = '.';
+		f.len_till_dot++;
+	}
+	else
+	{
+		f.str[f.len_till_dot--] = '.';
+		while (f.i <= f.len_till_dot)
+		{
+			f.str[f.len_till_dot] = f.inter % 10 + '0';
+			f.inter /= 10;
+			f.len_till_dot--;
+		}
+	}
+	return (ft_fill_fraction(f.fraction, f.str, p, f.len_af_dot));
 }
 
-int main() {
-    float a;
-    char  *b;
-    a = 11;
-    b = ft_ftoa(a, 8);
-    printf("%s\n", b);
-    printf("%1.8f\n", a);
-    return 0;
+int	main(void)
+{
+	float			a;
+	char			*b;
+
+	a = 213.11;
+	b = ft_ftoa(a, 6);
+	printf("%s\n", b);
+	printf("%1.6f\n", a);
+	return (0);
 }
