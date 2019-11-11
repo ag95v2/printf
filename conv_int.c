03bc32075358bd935f7e4186b9eb2c40e783384c
anton <goloviznin1995@yandex.ru>
2019-11-11 20:53:58 +0300

    read_spec refactored
    Moved read_spec to another file
    Changed read_length (now we support multiple length specifiers)
    Fixed Behrooz typos in conv_spec.c (Do not commit to master until your code compiles!)

--- conv_int.c
+++ conv_int.c
@@ -10,35 +10,35 @@ char	*itoa_b10u(long long n)
 {
 	if (n < 0)
 		n = n * (-1);
-	return (ft_itoa_base(long long n, 10));
+	return (ft_itoa_base( n, 10));
 }
 
 char	*itoa_b10(long long n)
 {
 	// Your code here
 	// Convert long long to string base 10
-	return (ft_itoa_base(long long n, 10));
+	return (ft_itoa_base( n, 10));
 }
 
 char	*itoa_b2(long long n)
 {
 	// Your code here
 	// Convert long long to string base 2
-	return (ft_itoa_base(long long n, 2));
+	return (ft_itoa_base( n, 2));
 }
 
 char	*itoa_b8(long long n)
 {
 	// Your code here
 	// Convert long long to string base 8
-	return (ft_itoa_base(long long n, 8));
+	return (ft_itoa_base( n, 8));
 }
 
 char	*itoa_bX(long long n)
 {
 	// Your code here
 	// Convert long long to string base hex in uppercase
-	return (ft_itoa_base(long long n, 16));
+	return (ft_itoa_base( n, 16));
 }
 
 char	*itoa_bx(long long value)
--- tests/printf/main.c
+++ tests/printf/main.c
@@ -10,11 +10,13 @@ int main()
 	ft_printf("Test4 (multiple args): arg1:%s arg2:%s\n", "hello", "world");
 	ft_printf("\nUsing stub functions (yet not implemented)\n");
 	ft_printf("Test4 (integer): %i\n", 123);
-	ft_printf("Test5 (decimal): %d\n", 123);
+	ft_printf("Test5 (decimal): %d\n", 123123);
 	ft_printf("Test6 (float): %f\n", 123.45);
+	ft_printf("Test6.1 (float): %f\n", 1123.456);
 	ft_printf("Test7 (hex): %x\n", 123);
+	ft_printf("Test7.1 (hex): %x\n", 1);
 	ft_printf("Test8 (HEX): %X\n", 123);
-	ft_printf("Test9 (octal): %o\n", 123);
+	ft_printf("Test9 (octal): %o\n", 9);
 	ft_printf("Test10 (pointer): %p\n", 123);
 	
 	return (0);
