56ef9107f8469a7c660aed9186062a165f80d963
dpenney <behaqosimi@gmail.com>
2019-11-13 20:41:46 +0300

    Changed types in ft_num_len and ft_itoa_base

--- libft/ft_num_len.c
+++ libft/ft_num_len.c
@@ -1,12 +1,10 @@
 #include "libft.h"
 
-int         ft_num_len(long long num, int base)
+int         ft_num_len(unsigned long long num, int base)
 {
     int     i;
 
     i = 0;
-    if (num < 0)
-        i++;
     while (num > 0)
     {
         num = num / base;
