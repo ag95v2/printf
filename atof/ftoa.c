4fc9ade57bd5e4b629448d9fc423701445aa8ae2
dpenney <behaqosimi@gmail.com>
2019-12-04 17:17:18 +0300

    testing new ftoa

--- atof/ftoa.c
+++ atof/ftoa.c
@@ -76,3 +76,11 @@ char	*ftoa(long double x)
 	else
 		return (normal_case(&num));
 }
+
+int		main(void)
+{
+	double a = 0.12345789;
+	printf("ORIGINAL: %f\n", a);
+	printf("%s\n", ftoa(a));
+	return(0);
+}
